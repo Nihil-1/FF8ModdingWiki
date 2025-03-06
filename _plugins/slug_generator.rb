7d01a350d24b37ef1a031963a223a3b98299e31d
HobbitDur <ludovic791@gmail.com>
2025-03-06 22:39:19 +0100

    Changing link

--- _plugins/slug_generator.rb
+++ _plugins/slug_generator.rb
@@ -1,6 +1,18 @@
-Jekyll::Hooks.register :pages, :pre_render do |page|
-  if page.data["title"]
-    slug = page.data["title"].downcase.strip.gsub(" ", "-").gsub(/[^\w-]/, '')
-    page.data["permalink"] = "/#{slug}/"
+module Jekyll
+  module PermalinkGenerator
+    def generate_permalink(title)
+      # Convert title to lowercase and replace spaces with hyphens
+      title.downcase.gsub(/\s+/, '-')
+    end
+  end
+
+  class Page
+    include PermalinkGenerator
+
+    def permalink
+      # Generate permalink from title if not explicitly set
+      return data['permalink'] if data['permalink']
+      "/#{generate_permalink(data['title'])}/"
+    end
   end
 end
